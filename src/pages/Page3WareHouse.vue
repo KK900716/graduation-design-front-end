965603ae73d57059ed670d49c7d446eaf83a88cd
HPME <443808626@qq.com>
2022-03-13 19:38:09 +0800

    3.12

--- src/pages/Page3WareHouse.vue
+++ src/pages/Page3WareHouse.vue
@@ -9,35 +9,43 @@
       <li></li>
     </ul>
   </div>
-
   <div class="content_title">上传下载</div>
   <div class="content_two">
     <div class="content_two_upload">
       <el-upload
-        class="upload-demo"
-        ref="upload"
-        action="https://jsonplaceholder.typicode.com/posts/"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :file-list="fileList"
-        :auto-upload="false">
+          class="upload-demo"
+          ref="upload"
+          action="http://127.0.0.1/upload"
+          :on-preview="handlePreview"
+          :on-remove="handleRemove"
+          :on-success="handlerSuccess"
+          :on-error="handlerError"
+          :file-list="fileList"
+          :auto-upload="false">
         <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
         <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
-        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+        <div slot="tip" class="el-upload__tip">请上传要处理的图片</div>
       </el-upload>
+<!--                :headers="headerMessage"-->
+<!--      <input name="file" type="file" accept="image/png,image/gif,image/jpeg" @change="update"/>-->
     </div>
-  </div>
   <div class="content_title">处理结果预览</div>
+  </div>
 </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Page3WareHouse",
   props: ['name'],
   data() {
     return {
-      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+      headerMessage:{
+        token:window.localStorage.getItem('access-admin')
+      },
+      fileList: []
     };
   },
   methods: {
@@ -48,7 +56,37 @@ export default {
       console.log(file, fileList);
     },
     handlePreview(file) {
+      console.log(this.fileList)
       console.log(file);
+    },
+    handlerSuccess(response, file, fileList){
+
+    },
+    handlerError(err, file, fileList){
+
+    },
+    update(e){
+      let self = this
+      let file = e.target.files[0]
+      /* eslint-disable no-undef */
+      let param = new FormData()  // 创建form对象
+      param.append('file', file)  // 通过append向form对象添加数据
+      param.append('chunk', '0') // 添加form表单中其他数据
+      console.log(param.get('file')) // FormData私有类对象，访问不到，可以通过get判断值是否传进去
+      let config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          token: window.localStorage.getItem('access-admin')
+        }
+      }
+      // 添加请求头
+      axios.post('http://127.0.0.1/upload', param, config)
+          .then(res => {
+            // if (res.data.code === 0) {
+            //   self.ImgUrl = res.data.data
+            // }
+            // console.log(res.data)
+          })
     }
   }
 }
