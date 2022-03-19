480e0f0e36b4d99d708bc29eb34b1954394615db
HPME <443808626@qq.com>
2022-03-19 14:52:30 +0800

    3.12

--- src/pages/Click2page.vue
+++ src/pages/Click2page.vue
@@ -8,8 +8,10 @@
       <div class="two_left">
         <div class="two_left_title">关于我们</div>
         <div class="two_left_content">
-          <div class="paragraph">XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>
-          <div class="paragraph">XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>
+          <div class="paragraph">石河子大学2020级计算机科学与技术专业（二学位）梁嘉成毕业设计作品：不同水体识别系统设计与实现。</div>
+          <div class="paragraph">本项目采用前端Vue+后端Spring Boot的前后端分离模式设计，前后端采用AJAX异步通信，完成信息间的交互工作，数据库选用Redis缓存技术+MySQL数据库，大大提高用户体验的同时保证了数据的安全性，服务层采用并发设计基于NIO模式，再次提升了用户的使用体验。</div>
+<!--          TODO 算法内容应该补充-->
+          <div class="paragraph">图像识别采用开源库OpenCV和成熟的Pytorch框架，结合XXXXX算法等，实现了对不同水体的精准图像识别。</div>
         </div>
       </div>
       <div class="two_right">
@@ -24,8 +26,8 @@
     <div class="three_content">
       <div class="three_content_title">为用户提供服务</div>
       <div class="three_content_content">
-        <div class="paragraph">XXX不同水体识别系统旨在为用户提供一个用于进行卫星、遥感图像水体识别的系统。</div>
-        <div class="paragraph">XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>
+        <div class="paragraph">不同水体识别系统旨在为用户提供一个用于进行卫星、遥感图像水体识别的系统。</div>
+        <div class="paragraph">本系统是一个付费软件，每个注册的用户都拥有自己的图片仓库，用户仅需上传图片等待一段时间，即可完成图像识别的工作，如果一个工作日内没有得到结果或得到的结果不理想，请及时联系工作人员！</div>
       </div>
     </div>
   </div>
